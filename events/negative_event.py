3cf006317d288d2b4466ef2513d35dc8b22a4de5
איתי בן חיים <thh123321@gmail.com>
2025-07-04 17:13:29 +0300

    Feat: added negative event

--- events/negative_event.py
+++ events/negative_event.py
@@ -1,11 +1,11 @@
 from context import Context
 from events.event import Event
-from events.severity import SEVERITY_DAYS_DICE, SEVERITY_RANGES, SEVERITY_SCORE
+from events.severity import SEVERITY_DAYS_DICE, SEVERITY_RANGES, SEVERITY_SCORE, Severity
 from outcome import Outcome
-from random_utils import choose_with_ranges
+from random_utils import Dice, choose_with_ranges
 
 
-class PositiveEvent(Event):
+class NegativeEvent(Event):
     def __init__(self, name: str) -> None:
         super().__init__()
         self.name = name
@@ -14,10 +14,20 @@ class PositiveEvent(Event):
         severity = self._get_severity()
         return Outcome(
             f"{self.name} ({severity.name.title()})",
-            SEVERITY_SCORE[severity],
+            SEVERITY_SCORE[severity] * (-1),
             SEVERITY_DAYS_DICE[severity].roll(),
-            did_initiative_change=False,
+            self._did_initiative_change(severity),
         )
+
+    @staticmethod
+    def _did_initiative_change(severity: Severity):
+        if severity is Severity.MINOR:
+            return False
+
+        if severity is Severity.MODERATE:
+            return Dice(2).roll() == 1
+
+        return True
 
     @staticmethod
     def _get_severity():
